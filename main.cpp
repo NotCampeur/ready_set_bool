6dcf5fd61d1e3877efe73c4a9f046fe0e3192b26
ldutriez-Inspiron <ldutriez@student.42.fr>
2023-02-09 13:14:16 +0100

    ✨ Loop through the test as long as the user want with lambda expression.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 15:06:44 by ldutriez          #+#    #+#             */
-/*   Updated: 2023/02/08 17:26:03 by ldutriez         ###   ########.fr       */
+/*   Updated: 2023/02/09 13:13:45 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,26 @@ unsigned int adder(unsigned int a, unsigned int b);
 
 void adder_test(void)
 {
-	std::cout << "Testing adder:\n";
+	std::cout << "Testing adder:\n"
+			<< "\tpress l to keep testing this module, n to move to the next one\n";
 
-	unsigned int a = rand() % 100;
-	unsigned int b = rand() % 100;
-	unsigned int result = adder(a, b);
-	std::cout << a << " + " << b << " = " << result << " (expected: " << a + b << ")\n";
+	unsigned int a, b, result;
+	auto tester = [&](void) -> void
+	{
+		a = rand() % 100;
+		b = rand() % 100;
+		result = adder(a, b);
+		std::cout << '\r' << a << " + " << b << " = " << result << " (expected: " << a + b << ")\n";
+	};
+
+	for (std::string input; input != "n" && std::cin.fail() == false;)
+	{
+		std::getline(std::cin, input);
+		if (input == "l")
+			tester();
+		else if (input != "n")
+			std::cout << "Invalid command\n";
+	}
 }
 
 void seed_selection(void)
