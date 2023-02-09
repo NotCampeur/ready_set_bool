e63771e00bfd8e101b71d65cc9521df24c24aa40
ldutriez-Inspiron <ldutriez@student.42.fr>
2023-02-09 14:29:05 +0100

    ✨✅🐛 Add carry handle for adder. Now the tests are up to unsigned int max. Now enter is used to iterate through tests.

--- adder/adder.cpp
+++ adder/adder.cpp
@@ -6,7 +6,7 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 15:06:48 by ldutriez          #+#    #+#             */
-/*   Updated: 2023/02/08 17:38:13 by ldutriez         ###   ########.fr       */
+/*   Updated: 2023/02/09 14:25:54 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@ unsigned int adder(unsigned int a, unsigned int b)
 {
 	unsigned int result(0);
 	unsigned int tmp(0);
+	unsigned int carry(0);
 
 	for (unsigned int i(0); i < 32 ; ++i)
 	{
 		tmp = 1 << i;
-		result |= (a & tmp) ^ (b & tmp);
+		result = result | ((a & tmp) ^ (b & tmp) ^ carry);
+		carry = ((a & tmp) & (b & tmp)) | ((a & tmp) & carry) | ((b & tmp) & carry);
+		carry = carry << 1;
 	}
 	return result;
 }
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 15:06:44 by ldutriez          #+#    #+#             */
-/*   Updated: 2023/02/09 13:13:45 by ldutriez         ###   ########.fr       */
+/*   Updated: 2023/02/09 14:27:13 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,31 @@
 #include <cstdlib>
 #include <cmath>
 #include <string>
+#include <limits>
 
 unsigned int adder(unsigned int a, unsigned int b);
 
 void adder_test(void)
 {
 	std::cout << "Testing adder:\n"
-			<< "\tpress l to keep testing this module, n to move to the next one\n";
+			<< "\tpress enter to keep testing this module, n to move to the next one\n";
 
 	unsigned int a, b, result;
 	auto tester = [&](void) -> void
 	{
-		a = rand() % 100;
-		b = rand() % 100;
+		a = rand() % std::numeric_limits<unsigned int>::max();
+		b = rand() % std::numeric_limits<unsigned int>::max();
 		result = adder(a, b);
-		std::cout << '\r' << a << " + " << b << " = " << result << " (expected: " << a + b << ")\n";
+		std::cout << a << " + " << b << " = " << result << " (expected: " << a + b << ")\n";
 	};
 
 	for (std::string input; input != "n" && std::cin.fail() == false;)
 	{
 		std::getline(std::cin, input);
-		if (input == "l")
+		if (input.empty() == true)
 			tester();
 		else if (input != "n")
-			std::cout << "Invalid command\n";
+			std::cout << "Invalid command : " << input << "\n";
 	}
 }
 
