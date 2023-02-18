02207edb957ec6d87ed19f8c46635aa61ee4c7f3
Lucas DUTRIEZ <ldutriez@e1r3p4.clusters.42paris.fr>
2023-02-18 17:22:21 +0100

    ✨ Add pointer to parent to ease ast handling.

--- includes/rpnAst/rpn_ast_node.hpp
+++ includes/rpnAst/rpn_ast_node.hpp
@@ -6,7 +6,7 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 16:16:08 by ldutriez          #+#    #+#             */
-/*   Updated: 2023/02/16 15:09:02 by ldutriez         ###   ########.fr       */
+/*   Updated: 2023/02/18 17:18:22 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ namespace rsb
 	class node
 	{
 		public:
-			node(const T & p_data) : data(p_data), left(nullptr), right(nullptr)
+			node(const T & p_data) : data(p_data), parent(nullptr), left(nullptr), right(nullptr)
 			{}
 			
 			node(const node<T> & to_copy)
-			: data(to_copy.data)
+			: data(to_copy.data), parent(nullptr)
 			, left(to_copy.left->clone()), right(to_copy.right.clone())
 			{}
 
@@ -38,6 +38,7 @@ namespace rsb
 				if (this != &to_assign)
 				{
 					data = to_assign.data;
+					parent = nullptr;
 					left = to_assign.left.clone();
 					right = to_assign.right.clone();
 				}
@@ -46,6 +47,7 @@ namespace rsb
 
 			// Return a string representation of the node and its children
 			// Used to build a formula from the tree
+			// Parents are ignored.
 			std::string formula(void) const
 			{
 				std::string result;
@@ -60,18 +62,27 @@ namespace rsb
 
 			// Return an allocated copy of the node and its children
 			// Used for rpn_abstract_syntax_tree's deep copy
+			// To avoid clunky behavior, the node from which the copy is made
+			// is considered the root of the new tree and its parent is not set.
 			node<T> *clone(void) const
 			{
 				node<T> *result(new node<T>(data));
 
 				if (left != nullptr)
+				{
 					result->left = left->clone();
+					result->left->parent = result;
+				}
 				if (right != nullptr)
+				{
 					result->right = right->clone();
+					result->right->parent = result;
+				}
 				return result;
 			}
 
 			T			data;
+			node<T>		*parent;
 			node<T>		*left;
 			node<T>		*right;
 	};
