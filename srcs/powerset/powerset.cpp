e9a66a0d983cd7b3e8c7ffafad76ec605e0c2858
Lucas DUTRIEZ <ldutriez@e1r13p14.clusters.42paris.fr>
2023-03-08 15:24:11 +0100

    ⚡️🎨 Clean and optimization of powerset.

--- srcs/powerset/powerset.cpp
+++ srcs/powerset/powerset.cpp
@@ -6,7 +6,7 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 19:01:22 by ldutriez          #+#    #+#             */
-/*   Updated: 2023/03/06 19:18:44 by ldutriez         ###   ########.fr       */
+/*   Updated: 2023/03/08 15:19:19 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,16 @@ std::vector<std::vector<__INT32_TYPE__> > powerset(std::vector<__INT32_TYPE__> s
 {
 	std::vector<std::vector<__INT32_TYPE__> >	powerset;
 	std::vector<__INT32_TYPE__>					subset;
-	__INT32_TYPE__								i;
-	std::vector<__INT32_TYPE__>::size_type		j;
 
-	i = 0;
-	while (i < (1 << set.size()))
+	for (__INT32_TYPE__ i(0), powerset_size(1 << set.size());
+		i < powerset_size;
+		++i)
 	{
-		j = 0;
-		while (j < set.size())
-		{
+		for (std::vector<__INT32_TYPE__>::size_type j(0); j < set.size(); ++j)
 			if (i & (1 << j))
 				subset.push_back(set[j]);
-			j++;
-		}
 		powerset.push_back(subset);
 		subset.clear();
-		i++;
 	}
 	return (powerset);
 }
