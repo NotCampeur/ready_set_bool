f2d353ba87769b126f790f7b331ed8fc7755db50
Lucas DUTRIEZ <ldutriez@e2r2p4.clusters.42paris.fr>
2023-03-15 20:46:16 +0100

    ✨✅🚧 Add of Z-order curve and tests. It is not yet in the closed interval [0;1].

--- srcs/spaceFillingCurve/map.cpp
+++ srcs/spaceFillingCurve/map.cpp
@@ -6,15 +6,30 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 16:45:52 by ldutriez          #+#    #+#             */
-/*   Updated: 2023/03/15 17:38:37 by ldutriez         ###   ########.fr       */
+/*   Updated: 2023/03/15 20:35:54 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ready_set_bool.hpp"
+#include <cmath>
 
+// Z-order curve bijective function that map multidimensional data to
+// one dimension while preserving locality of the data points in
+// the closed interval [0; 1]
 double map(uint16_t x, uint16_t y)
 {
-	(void)(x);
-	(void)(y);
-	return 0;
+	double result = 0;
+	uint32_t int_result(0);
+
+	for (uint8_t i(0); i < 16; ++i)
+	{
+		int_result = int_result | ((x & (1 << i)) << i);
+	}
+	for (uint8_t i(0); i < 16; ++i)
+	{
+		int_result = int_result | ((y & (1 << i)) << (i + 1));
+	}
+	result = int_result;
+	return result;
+	// return result / std::pow(2, 32);
 }
--- srcs/spaceFillingCurve/map_tests.cpp
+++ srcs/spaceFillingCurve/map_tests.cpp
@@ -6,20 +6,38 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 16:40:46 by ldutriez          #+#    #+#             */
-/*   Updated: 2023/03/15 17:47:06 by ldutriez         ###   ########.fr       */
+/*   Updated: 2023/03/15 20:42:04 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ready_set_bool.hpp"
+#include <limits>
 
 void map_test(void)
 {
 	std::cout << "Testing \033[1;37mmap\033[0m:\n"
 			<< "\tpress \033[1;36menter\033[0m to keep testing this module, \033[1;36mn\033[0m to move to the next one\n";
 
+	uint16_t x, y = 0;
+	double result = 0;
+	
+	auto get_spatial_data = [&](uint16_t &x, uint16_t &y) -> void
+	{
+		std::cout << "Enter the x \033[1;36mcoordinate [0;" << std::numeric_limits<uint16_t>::max() << "]\033[0m : ";
+		std::cin >> x;
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cin.clear();
+		std::cout << "Enter the y \033[1;36mcoordinate [0;" << std::numeric_limits<uint16_t>::max() << "]\033[0m : ";
+		std::cin >> y;
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cin.clear(); 
+	};
+
 	auto tester = [&](void) -> void
 	{
-		std::cout << "Not implemented yet\n";
+		get_spatial_data(x, y);
+		result = map(x, y);
+		std::cout << "The reverse space filling curve of {x=" << x << ", y=" << y << "} is: " << result << "\n";
 	};
 
 	for (std::string input; input != "n" && std::cin.fail() == false;)
