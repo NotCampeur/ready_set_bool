78b2faa4167d93be527159014ef0ea0d33f01800
Lucas DUTRIEZ <ldutriez@e1r10p14.clusters.42paris.fr>
2023-03-17 17:55:47 +0100

    🚸🔊 Sanitize input handling and color error output.

--- srcs/spaceFillingCurve/map/map_tests.cpp
+++ srcs/spaceFillingCurve/map/map_tests.cpp
@@ -6,7 +6,7 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 16:40:46 by ldutriez          #+#    #+#             */
-/*   Updated: 2023/03/15 20:58:40 by ldutriez         ###   ########.fr       */
+/*   Updated: 2023/03/17 17:54:00 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,31 @@ void map_test(void)
 	uint16_t x, y = 0;
 	double result = 0;
 	
-	auto get_spatial_data = [&](uint16_t &x, uint16_t &y) -> void
+	auto get_spatial_data = [&](uint16_t &x, uint16_t &y) -> bool
 	{
 		std::cout << "Enter the x \033[1;36mcoordinate [0;" << std::numeric_limits<uint16_t>::max() << "]\033[0m : ";
 		std::cin >> x;
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		std::cin.clear();
+		if (std::cin.eof() == true || std::cin.fail() == true)
+		{
+			std::cin.clear();
+			return false;
+		}
 		std::cout << "Enter the y \033[1;36mcoordinate [0;" << std::numeric_limits<uint16_t>::max() << "]\033[0m : ";
 		std::cin >> y;
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		std::cin.clear(); 
+		if (std::cin.eof() == true || std::cin.fail() == true)
+		{
+			std::cin.clear(); 
+			return false;
+		}
+		return true;
 	};
 
 	auto tester = [&](void) -> void
 	{
-		get_spatial_data(x, y);
+		if (get_spatial_data(x, y) == false)
+			return;
 		result = map(x, y);
 		std::cout.precision(std::numeric_limits<double>::max_digits10);
 		std::cout << "The reverse space filling curve of {x=" << x << ", y=" << y << "} is: " << std::fixed << result << "\n";
@@ -44,9 +54,9 @@ void map_test(void)
 	for (std::string input; input != "n" && std::cin.fail() == false;)
 	{
 		std::getline(std::cin, input);
-		if (input.empty() == true)
+		if (std::cin.fail() == false && input.empty() == true)
 			tester();
 		else if (input != "n")
-			std::cout << "Invalid command : " << input << "\n";
+			std::cout << "\033[0;31mInvalid command : " << input << "\033[0m\n";
 	}
 }
