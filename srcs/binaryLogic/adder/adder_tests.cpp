e0ac349eb19971adad034d2354ad603965ec724b
Lucas DUTRIEZ <ldutriez@e1r7p6.clusters.42paris.fr>
2023-03-20 17:23:09 +0100

    🚸 Add input handling to test adder.

--- srcs/binaryLogic/adder/adder_tests.cpp
+++ srcs/binaryLogic/adder/adder_tests.cpp
@@ -6,7 +6,7 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 14:54:30 by ldutriez          #+#    #+#             */
-/*   Updated: 2023/03/20 17:03:27 by ldutriez         ###   ########.fr       */
+/*   Updated: 2023/03/20 17:21:23 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,32 @@ void adder_test(void)
 			<< "\tpress " B_CYAN "enter" NORMAL " to keep testing this module, " B_CYAN "n" NORMAL " to move to the next one\n";
 
 	uint32_t a, b, result;
+
+	auto get_operand = [&](void) -> uint32_t
+	{
+		std::string input;
+		while (std::cin.fail() == false)
+		{
+			std::cout << "Enter an " B_CYAN "operand" NORMAL " (empty for random): ";
+			std::getline(std::cin, input);
+			if (input.empty() == true && std::cin.fail() == false)
+				return rand() % std::numeric_limits<uint32_t>::max();
+			try
+			{
+				return std::stoul(input);
+			}
+			catch(...)
+			{
+				std::cout << RED "Invalid input" NORMAL "\n";
+			}
+		}
+		return 0;
+	};
+	
 	auto tester = [&](void) -> void
 	{
-		a = rand() % std::numeric_limits<uint32_t>::max();
-		b = rand() % std::numeric_limits<uint32_t>::max();
+		a = get_operand();
+		b = get_operand();
 		result = adder(a, b);
 		std::cout << a << " + " << b << " = " << result << " (expected: " << a + b << ")";
 		if (result == a + b)
