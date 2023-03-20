d97e2a80b9c2622588dbf7e93c1e1ce9a8e3987d
Lucas DUTRIEZ <ldutriez@e1r7p6.clusters.42paris.fr>
2023-03-20 19:35:57 +0100

    🚸 Add input handling to test multiplier.

--- srcs/binaryLogic/multiplier/multiplier_tests.cpp
+++ srcs/binaryLogic/multiplier/multiplier_tests.cpp
@@ -6,7 +6,7 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 14:59:04 by ldutriez          #+#    #+#             */
-/*   Updated: 2023/03/19 17:14:38 by ldutriez         ###   ########.fr       */
+/*   Updated: 2023/03/20 19:34:52 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,42 @@ void multiplier_test(void)
 			<< "\tpress " B_CYAN "enter" NORMAL " to keep testing this module, " B_CYAN "n" NORMAL " to move to the next one\n";
 
 	uint32_t a, b, result;
+
+	auto get_operand = [&](void) -> uint32_t
+	{
+		std::string input;
+		while (std::cin.fail() == false)
+		{
+			std::cout << "Enter an " B_CYAN "operand" NORMAL " (empty for random): ";
+			std::getline(std::cin, input);
+			if (input.empty() == true && std::cin.fail() == false)
+			{
+				uint32_t random_result(rand() % 1000);
+				std::cout << B_WHITE << random_result << NORMAL "\n";
+				return random_result;
+			}
+			try
+			{
+				return std::stoul(input);
+			}
+			catch(...)
+			{
+				std::cout << RED "Invalid input" NORMAL "\n";
+			}
+		}
+		return 0;
+	};
+
 	auto tester = [&](void) -> void
 	{
-		a = rand() % 100;
-		b = rand() % 100;
+		a = get_operand();
+		b = get_operand();
 		result = multiplier(a, b);
-		std::cout << a << " * " << b << " = " << result << " (expected: " << a * b << ")\n";
+		std::cout << a << " * " << b << " = " << result << " (expected: " << a * b << ")";
+		if (result == a * b)
+			std::cout << " " GREEN "OK" NORMAL "\n";
+		else
+			std::cout << " " RED "KO" NORMAL "\n";
 	};
 
 	for (std::string input; input != "n" && std::cin.fail() == false;)
@@ -32,6 +62,6 @@ void multiplier_test(void)
 		if (input.empty() == true)
 			tester();
 		else if (input != "n")
-			std::cout << "Invalid command : " << input << "\n";
+			std::cout << RED "Invalid input" NORMAL "\n";
 	}
 }
