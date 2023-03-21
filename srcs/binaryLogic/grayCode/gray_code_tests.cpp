8066a0cc94bfad5e3af1c84d2bf7a1fb0ffe81da
Lucas DUTRIEZ <ldutriez@e1r4p8.clusters.42paris.fr>
2023-03-21 15:04:15 +0100

    🚸 Add input handling to test gray code.

--- srcs/binaryLogic/grayCode/gray_code_tests.cpp
+++ srcs/binaryLogic/grayCode/gray_code_tests.cpp
@@ -6,7 +6,7 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 15:23:08 by ldutriez          #+#    #+#             */
-/*   Updated: 2023/03/19 17:14:38 by ldutriez         ###   ########.fr       */
+/*   Updated: 2023/03/21 15:02:59 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,37 @@ void gray_code_test(void)
 			<< "\tpress " B_CYAN "enter" NORMAL " to keep testing this module, " B_CYAN "n" NORMAL " to move to the next one\n";
 
 	uint32_t a, result;
+
+	auto get_operand = [&](void) -> uint32_t
+	{
+		std::string input;
+		while (std::cin.fail() == false)
+		{
+			std::cout << "Enter an " B_CYAN "operand" NORMAL " (empty for random): ";
+			std::getline(std::cin, input);
+			if (input.empty() == true && std::cin.fail() == false)
+			{
+				uint32_t random_result(rand() % std::numeric_limits<uint32_t>::max());
+				std::cout << B_WHITE << random_result << NORMAL "\n";
+				return random_result;
+			}
+			try
+			{
+				return std::stoul(input);
+			}
+			catch(...)
+			{
+				std::cout << RED "Invalid input" NORMAL "\n";
+			}
+		}
+		return 0;
+	};
+	
 	auto tester = [&](void) -> void
 	{
-		a = rand() % 100;
+		a = get_operand();
 		result = gray_code(a);
-		std::cout << "Gray code of " << a << " = " << result << "\n";
+		std::cout << "Gray code of " << a << " = " << result << '\n';
 	};
 
 	for (std::string input; input != "n" && std::cin.fail() == false;)
@@ -32,6 +58,6 @@ void gray_code_test(void)
 		if (input.empty() == true)
 			tester();
 		else if (input != "n")
-			std::cout << "Invalid command : " << input << "\n";
+			std::cout << RED "Invalid input" NORMAL "\n";
 	}
 }
