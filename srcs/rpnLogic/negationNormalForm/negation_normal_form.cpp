3d6319e83733ce292bfc8de73110edb7c7c77044
Lucas DUTRIEZ <ldutriez@e1r4p8.clusters.42paris.fr>
2023-03-21 17:24:35 +0100

    🚸 negation_normal_form now return the formula in case of wrong input. Add color to error returns.

--- srcs/rpnLogic/negationNormalForm/negation_normal_form.cpp
+++ srcs/rpnLogic/negationNormalForm/negation_normal_form.cpp
@@ -6,28 +6,27 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 16:05:23 by ldutriez          #+#    #+#             */
-/*   Updated: 2023/02/16 17:52:47 by ldutriez         ###   ########.fr       */
+/*   Updated: 2023/03/21 17:23:23 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "ready_set_bool.hpp"
 #include "rpn_abstract_syntax_tree.hpp"
 
 std::string negation_normal_form(const std::string &formula)
 {
-	std::string result;
 	rsb::rpn_abstract_syntax_tree<char, rsb::variable_boolean_token_check> tree;
 
 	try
 	{
 		tree.build(std::vector<char>(formula.begin(), formula.end()));
 		tree.negation_normal_form();
-		result = tree.formula();
+		return tree.formula();
 	}
 	catch(const std::exception& e)
 	{
-		std::cerr << e.what() << '\n';
+		std::cerr << RED << e.what() << NORMAL "\n";
 	}
 
-	return result;
+	return formula;
 }
--- srcs/rpnLogic/negationNormalForm/negation_normal_form_tests.cpp
+++ srcs/rpnLogic/negationNormalForm/negation_normal_form_tests.cpp
@@ -6,7 +6,7 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 17:53:24 by ldutriez          #+#    #+#             */
-/*   Updated: 2023/03/19 17:14:38 by ldutriez         ###   ########.fr       */
+/*   Updated: 2023/03/21 17:22:10 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,6 @@ void negation_normal_form_test(void)
 		if (input.empty() == true)
 			tester();
 		else if (input != "n")
-			std::cout << "Invalid command : " << input << "\n";
+			std::cout << RED "Invalid input" NORMAL "\n";
 	}
 }
